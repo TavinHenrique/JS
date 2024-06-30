aaea94b9430bc37c85c3be02505a00158804369e
Octavio Henrique <alvesoctavio11@gamil.com>
2024-06-30 16:09:12 -0300

    aula11

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel=70
+console.log(`a sua velocidade foi ${vel}`)
+if(vel>60){
+    console.log(`Voce ultrapassou aa velocidade permitida, MULTADO`)
+ }
+console.log(`dirija com cuidado`)
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais='EUA'
+if(pais=='brasil'){
+    console.log('Voce é brasileiro')
+}
+else{
+    console.log('Voce é estrangeiro')
+}
