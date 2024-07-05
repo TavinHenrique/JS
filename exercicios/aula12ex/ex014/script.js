c04e407592cc329fe9a8b9de8a874eb25af15efb
Octavio Henrique <alvesoctavio11@gamil.com>
2024-07-04 23:23:37 -0300

    refazendo os exercicios

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -1,20 +1,19 @@
 function carregar(){
-    var msg=document.querySelector('div#msg')
-    var img=document.querySelector('img#imagem')
-    var data=new Date()
+    var data= new Date()
     var hora=data.getHours()
     //var hora=10
+    var msg=document.querySelector('div#msg')
+    var img=document.querySelector('img#imagem')
     msg.innerHTML=`Agora são ${hora} horas`
-    if(hora>=0 && hora<12){
+    if(hora>=6 && hora<12){
         img.src='imagens/manha.jpg'
-        document.body.style.background='#FFB0B8'
+        document.body.style.background='#FCB2BD'
     }else if(hora>=12 && hora<18){
         img.src='imagens/tarde.jpg'
-        document.body.style.background='#FEC5A6'
-    }
-    else{
-        img.src='imagens/noite.jpg'
-        document.style.body.background='#071D1B'
-    }
+        document.body.style.background='#F3B176'
         
+    }else{
+        img.src='imagens/noite.jpg'
+        document.body.style.background='#071C1F'
+    }
 }
--- exercicios/aula12ex/ex015/script.js
+++ exercicios/aula12ex/ex015/script.js
@@ -1,64 +1,51 @@
 function verificar(){
-    var data=new Date
-    var ano=data.getFullYear()
-    var fano=document.querySelector('input#txtano')
-    var res=document.querySelector('div#res')
-    if(fano.value.lenght==0|| Number(fano.value)>ano){
-        alert('[ERRO] Verique Novamente')
-    }else{
-        var sex=document.getElementsByName('sex')
-        var idade= ano-Number(fano.value)
+    var data= new Date()
+    var ano= data.getFullYear()
+    var fano=document.getElementById('txtano')
+    var msg =document.querySelector('div#msg')
+    if(fano.value.length==0 || Number(fano.value)>ano)
+        {alert('Verique os dados e tente novamente')}
+    else{
+        var fsex=document.getElementsByName('sex')
+        var idade= ano- Number(fano.value)
         var genero=""
-        var img=document.createElement('img')
-        img.setAttribute('id','foto')
-        if(sex[0].checked){
-            genero='masculino'
-            if(idade>=0 && idade<=13){
-                img.scr='imagens/menino.jpg'
-            }else if(idade<21){
-                img.scr='imagens/menino21.jpg'
-            }else if (idade<30){
-                img.scr='imagens/cara30 anos.jpg'
-            }else if (idade<55){
-                img.scr='imagens/veio55anos.jpg'
-            }
-        }else if(sex[1].checked){
-            genero='feminino'
-            if(idade>=0 && idade<=13){
-                img.scr='imagens/menina.jpg'
-            }else if(idade<21){
-                img.scr='imagens/menina21.jpg'
-            }else if (idade<30){
-                img.scr='imagens/mulher30anos.jpg'
-            }else if (idade<55){
-                img.scr='imagens/veia55anos.jpg'
-            }
+        var img=document.createElement('img')//cria elemento novo
+        img.setAttribute('id', 'foto')//coloca id,class,...
+        if(fsex[0].checked){
+            genero='homem'
+            if(idade>=0 && idade<=13)
+                {
+                    //criança
+                    img.setAttribute('src', 'imagens/menino.jpg')
+                }else if(idade<=24){
+                    //jovem
+                    img.setAttribute('src', 'imagens/menino21 (1).jpg')
+                }else if(idade<=45){
+                    //adulta
+                    img.setAttribute('src', 'imagens/cara30 anos.jpg')
+                }else{
+                    //velho
+                    img.setAttribute('src', 'imagens/veio55anos.jpg')
+                }
+        }else{
+            genero='mulher'
+            if(idade>=0 && idade<=13)
+                {
+                    //criança
+                    img.setAttribute('src', 'imagens/menina.jpg')
+                }else if(idade<=24){
+                    //jovem
+                    img.setAttribute('src', 'imagens/menina21.jpg')
+                }else if(idade<=45){
+                    //adulta
+                    img.setAttribute('src','imagens/mulher30anos.jpg')
+                }else{
+                    //velha
+                    img.setAttribute('src','imagens/veia55anos.jpg')
+                }
         }
-        res.style.textAlign='center'
-        res.innerHTML=`Você tem ${idade} anos e é do sexo ${genero}`
-        res.appendChild(img)
-    
+        msg.style.textAlign='center'
+        msg.innerHTML=`Você é ${genero} com ${idade} anos`
+        msg.appendChild(img)//adc imagem 
     }
-        
-    
-    
-    
-    /*if(calc>=0 && calc<=13 && fem){
-         img.scr='imagens/menina.jpg'
-    }else if( calc>=0 && calc<=13 && mas){
-         img.scr='imagens/menino.jpg'
-    }else if( calc>=14 && calc<=21 && fem){
-         img.scr='imagens/menina21.jpg'
-    }else if( calc>=14 && calc<=21 && mas){
-         img.scr='imagens/menino21.jpg'
-    }else if( calc>=22 && calc<=30 && fem){
-        img.scr='imagens/mulher30anos.jpg'
-    }else if(calc>=22 && calc<=30 && mas){
-        img.scr='imagens/cara30 anos.jpg'
-    }else if(calc>=31 && calc<=55 && fem){
-        img.scr='imagens/veia55anos.jpg'
-    }else if(calc>=31 && calc<=55 && mas){
-        img.scr='imagens/veio55anos.jpg'          var calc= ano-idade
-    res.innerHTML=`Sua idade é:${calc} e você é ${sex}`
-    }*/
 }
